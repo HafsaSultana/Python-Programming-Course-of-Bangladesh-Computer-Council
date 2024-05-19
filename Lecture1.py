fbb9041d7524e3b56dc176fd17a6b741b2950ab9
HafsaSultana <hafsa1720@cseku.ac.bd>
2024-05-19 12:14:26 +0600

    Add lecture1.py file

---
+++ Lecture1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 15 15:28:48 2024
+
+@author: Hafsa
+"""
+
+print("Computer Science and Engineering")
+print("Khulna University")
+print("Bangladesh")
+    
