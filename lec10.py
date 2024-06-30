00df09de5f84d7b04d28f27626214430b60c4151
HafsaSultana <hafsa1720@cseku.ac.bd>
2024-06-30 13:30:05 +0600

    Add lecture 10 python file

---
+++ lec10.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 17:20:02 2024
+
+@author: Hafsa
+
+for i in range(0,5):
+    for j in range(0,3):
+        print("*",end='')
+        
+    print()
+    
+  """
+  
+for i in range(0,5):
+    for j in range(0,4):
+        print(i,end='')
+        
+    print()
